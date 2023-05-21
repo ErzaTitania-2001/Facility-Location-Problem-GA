570bf7b9fdc45d914c3cc848c51fff4310388a35
ErzaTitania-2001 <lohoripal2001@gmail.com>
2023-05-21 21:47:44 +0530

    Removed material UI

--- frontend/src/pages/Client/ClientApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ClientApp = () => {
-  return (
-    <div>ClientApp</div>
-  )
-}
-
-export default ClientApp
--- frontend/src/pages/Pickup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Pickup = () => {
-  return (
-    <div>Pickup</div>
-  )
-}
-
-export default Pickup
